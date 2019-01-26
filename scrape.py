4c47c2b5b3301146e23ce31713110e207fdc15e3
jessicashu7 <jessicashu7@gmail.com>
2019-01-25 23:20:58 -0800

    have list of all courseblocks

--- scrape.py
+++ scrape.py
@@ -7,4 +7,10 @@ page_response = requests.get(page_url)
 
 if __name__ == "__main__":
     soup = BeautifulSoup(page_response.text, "html5lib")
-    print(soup.title)
+    titletag = soup.title
+    titlestr = str(titletag.string)
+    print(titlestr)
+
+    allcourses = soup.find_all("div", class_="courseblock")
+    for course in allcourses:
+        print(course.prettify())
