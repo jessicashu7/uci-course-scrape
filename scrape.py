8ce5bce29b3e8f2cb905931a61f1be1f8a3c512a
jessicashu7 <jessicashu7@gmail.com>
2019-03-27 15:19:54 -0700

    store all cs courses in database

--- scrape.py
+++ scrape.py
@@ -7,10 +7,14 @@ from firebase_admin import firestore
 page_url = "http://catalogue.uci.edu/allcourses/compsci/"
 page_response = requests.get(page_url)
 
+cred = credentials.Certificate('planeater-382fd1e9a9ee.json')
+firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
 if __name__ == "__main__":
     soup = BeautifulSoup(page_response.text, "html5lib")
+    #title  = str(soup.find("h1").string)
 
     allcourses = soup.find_all("div", class_="courseblock")
     for course in allcourses:
@@ -31,3 +35,13 @@ if __name__ == "__main__":
             if cd.string:
                 course_desc += str(cd.string) + "\n"
         print(course_desc)
+
+        #print(title)
+
+        doc_ref = db.collection(u'courselist').document(course_code)
+        doc_ref.set({
+            u'course_code': course_code,
+            u'course_name': course_name,
+            u'course_units': course_units,
+            u'course_desc': course_desc
+        })
