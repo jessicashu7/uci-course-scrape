d4dc65a52fe074c8bcceb453f6b43d5da1645977
jessicashu7 <jessicashu7@gmail.com>
2019-04-03 23:38:53 -0700

    changed to normalize \xa0

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
   var $input = $(".typeahead");
--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import requests
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import unicodedata
 
 page_url = "http://catalogue.uci.edu/allcourses/compsci/"
 page_response = requests.get(page_url)
@@ -12,16 +13,17 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+
 if __name__ == "__main__":
     soup = BeautifulSoup(page_response.text, "html5lib")
     #title  = str(soup.find("h1").string)
-
+    '''
     allcourses = soup.find_all("div", class_="courseblock")
     for course in allcourses:
-        course_block_title = str(course.find("p", class_="courseblocktitle").string)
+        course_block_title = unicodedata.normalize("NFKD", str(course.find("p", class_="courseblocktitle").string))
         course_info = course_block_title.split(".")
 
-        course_code = course_info[0].strip()
+        course_code = unicodedata.normalize('NFKD',course_info[0].strip())
         course_name = course_info[1].strip()
         course_units = course_info[2].strip().split(" ")[0]
         print(course_code)
@@ -34,14 +36,23 @@ if __name__ == "__main__":
         for cd in coursedesclist:
             if cd.string:
                 course_desc += str(cd.string) + "\n"
+        course_desc = unicodedata.normalize("NFKD", course_desc)
         print(course_desc)
 
         #print(title)
 
-        doc_ref = db.collection(u'courselist').document(course_code)
+        doc_ref = db.collection('courselist').document(course_code)
         doc_ref.set({
-            u'course_code': course_code,
-            u'course_name': course_name,
-            u'course_units': course_units,
-            u'course_desc': course_desc
+            'course_code': course_code,
+            'course_name': course_name,
+            'course_units': course_units,
+            'course_desc': course_desc
         })
+
+    '''
+    doc_ref = db.collection('courselist')
+    doc = doc_ref.get()
+    for d in doc:
+        dstring = u'{}: {}'.format(d.id,d.to_dict())
+        print(dstring)
+        print()
